07003a27a990ce112ba9cdabb55085214780569a
Jim Thomason <jim@japanesewartuba.com>
2020-01-29 13:51:25 -0600

    migrated to google-home-notify-client

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const GoogleHome = require('google-home-push');
+const { Device } = require('google-home-notify-client');
 const axios = require('axios');
+
+const devices = [
+  ['10.0.1.226', 'Living room'],
+  ['10.0.1.75', 'Basement'],
+  ['10.0.1.84', 'Bedroom'],
+  ['10.0.1.144', 'Dog room'],
+]
 
 const INTERVAL = 60 * 1000;
 //const INTERVAL = 500;
@@ -10,8 +17,6 @@ const STATUS_URL = 'http://localhost:3000/status';
 
 let status         = { washer : 'stopped', dryer : 'stopped' };
 let pendingMessage = undefined;
-
-const myHome = new GoogleHome('Living room Home');
 
 setInterval( () => {
   axios.get(STATUS_URL)
@@ -25,25 +30,23 @@ setInterval( () => {
       }
 
       if (newStatus.washer === 'stopped' && status.washer === 'running') {
-        messages.push('Washer is done');
+        for (let i; i < devices.length; i++) {
+          const device = new Device(...device)
+          await device.notify("Washer is done")
+        }
       }
 
       if (newStatus.dryer === 'stopped' && status.dryer === 'running') {
-        messages.push('Dryer is done');
+        for (let i; i < devices.length; i++) {
+          const device = new Device(...device)
+          await device.notify("Dryer is done")
+        }
       }
 
       status = newStatus;
 
       if (DEBUG) {
         console.log(`Current status : washer : ${status.washer} , dryer : ${status.dryer}`);
-      }
-
-      if (messages.length) {
-        const message = messages.join('. ');
-        try {
-          await myHome.speak(message);
-        }
-        catch(e) { pendingMessage = message };
       }
 
     })
